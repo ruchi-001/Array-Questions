fb061c081b964acb6210819dd8ea4af5df5fb6dd
ruchi-001 <ruchikumari2214@gmail.com>
2026-07-01 21:55:12 +0530

    Added solution of Best Time To Buy And Shell Stocks Problem

--- BuyAndShellStocks.java
+++ BuyAndShellStocks.java
@@ -15,3 +15,17 @@ class Solution {
        
     }
 }
+
+public static void main(String[] args) {
+       // Sample input
+       int[] prices = {7, 1, 5, 3, 6, 4};
+
+
+       Solution sol = new Solution();
+       int result = sol.maxProfit(prices);
+
+
+       System.out.println("Maximum Profit: " + result);
+   }
+}
+
